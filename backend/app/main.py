dfe49284bb7d49f3bacc83edb2f9764d5b32ff07
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-25 10:17:01 +0400

    use the user routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
+from app.routes import userRoutes
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI on macOS!"}
+app.include_router(userRoutes.router ,prefix="/users" ,tags=["Users"])
 
