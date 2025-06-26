5e482d6921da2f3aad0f64c1ae2aee5e0eba9288
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-26 08:40:34 +0400

    add documentation and login api

--- backend/app/routes/userRoutes.py
+++ backend/app/routes/userRoutes.py
@@ -5,15 +5,34 @@ from services import userServices
 router = APIRouter()
 
 # Register API
-@router.post("/register")
-async def register(user: UserCreate):
+# When university users try to login to the system:
+# 1. check our local database if the user already exsist in it , if not :
+#   - check the university users from the active directory
+#   - if exist : then fetch the user information
+#  
+@router.post("/login")
+async def login(user: UserCreate):
     # Check if email and password are provided
     if not user.email or not user.password:
         raise HTTPException(status_code=400, detail="Email and Password are required")
 
-    # Check if user already exists (assuming async DB call)
-    existing_user = await userServices.CheckUserExistence(user.email)
+    # Check if user already exists in local DB 
+    existing_user = await userServices.CheckUserExistenceDB(user.email)
     if existing_user:
-        raise HTTPException(status_code=400, detail="User already exists")
+        # generate a JWT
+        token = CreateJwtToken(existing_user.id, existing_user.email)
+        return {
+            "message": "Login successful from DB",
+            "token": token
+        }
+    elif not existing_user:
+        active_dir_user = await userServices.CheckUserExistenceAD(user.email)
+        # add user to local DB
+        token = CreateJwtToken(existing_user.id, existing_user.email)
+        return {
+            "message": "Login successful from AD",
+            "token": token
+        }
+        # raise HTTPException(status_code=400, detail="User already exists")
 
     return {"message": "User registered successfully"}
