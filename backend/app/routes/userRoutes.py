7ebf1201bcaf3f374db6ed99129bf6a2d9bbed66
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-28 14:23:46 +0400

    setup login route

--- backend/app/routes/userRoutes.py
+++ backend/app/routes/userRoutes.py
@@ -12,28 +12,37 @@ router = APIRouter()
 #  
 @router.post("/login")
 async def login(user: UserCreate):
-    # Check if email and password are provided
     if not user.email or not user.password:
         raise HTTPException(status_code=400, detail="Email and Password are required")
 
-    # Check if user already exists in local DB 
     existing_user = await userServices.CheckUserExistenceDB(user.email)
+
     if existing_user:
-        # generate a JWT
-        token = userServices.CreateJwtToken(existing_user.id, existing_user.email)
+        token = await userServices.CreateJwtToken(
+            existing_user["id"],
+            existing_user["email"],
+            existing_user["role"],
+            existing_user["first_name"],
+            existing_user["last_name"]
+        )
         return {
             "message": "Login successful from DB",
             "token": token
         }
-    elif not existing_user:
-        ActiveDirUser = await userServices.CheckUserExistenceAD(user.email)
-        # add user to local DB
-        userServices.CreateNewUserInDB(ActiveDirUser)
-        token = userServices.CreateJwtToken(existing_user.id, existing_user.email)
+
+    ad_user = await userServices.CheckUserExistenceAD(user.email)
+    if ad_user:
+        new_user = userServices.CreateNewUserInDB(ad_user)
+        token = await userServices.CreateJwtToken(
+            new_user["id"],
+            new_user["email"],
+            new_user["role"],
+            new_user["first_name"],
+            new_user["last_name"]
+        )
         return {
             "message": "Login successful from AD",
             "token": token
         }
-        # raise HTTPException(status_code=400, detail="User already exists")
 
-    return {"message": "User registered successfully"}
+    raise HTTPException(status_code=404, detail="User not found")
