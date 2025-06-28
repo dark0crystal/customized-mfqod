e702feb85ec5620b0127411fc0ffc3a6da3bc11b
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-28 15:33:19 +0400

    changes in user schema

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,11 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from enum import Enum
 
+
+# 1. Status Enum to restrict values
+class StatusEnum(str, Enum):
+    active = "active"
+    inactive = "inactive"
+    pending = "pending"
+    suspended = "suspended"
+
+
+# 2. Model for user login or auth-related operations
 class UserCreate(BaseModel):
-    user_id:str
-    email:str
-    password:str
-    first_name:str
-    last_name:str
-    role:str
+    user_id: str
+    email: EmailStr  
+    password: str
+    first_name: str
+    last_name: str
+    role: str
 
 
+# 3. Model for new user registration (outside uni)
+class UserRegister(BaseModel):
+    id: str
+    email: EmailStr
+    password: str
+    first_name: str
+    middle_name: str
+    last_name: str
+    phone_number: str
+    status: StatusEnum
