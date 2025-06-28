6fe27d9d0b302b84f16a81a3402b246614645343
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-28 22:29:47 +0400

    fix types

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from enum import Enum
 
 
@@ -13,7 +13,7 @@ class StatusEnum(str, Enum):
 # 2. Model for user login or auth-related operations
 class UserCreate(BaseModel):
     user_id: str
-    email: EmailStr  
+    email: str  
     password: str
     first_name: str
     last_name: str
@@ -23,7 +23,7 @@ class UserCreate(BaseModel):
 # 3. Model for new user registration (outside uni)
 class UserRegister(BaseModel):
     id: str
-    email: EmailStr
+    email: str
     password: str
     first_name: str
     middle_name: str
