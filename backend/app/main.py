f59677577197b3f12cc708d1f45e8602f502cc1d
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-28 15:33:05 +0400

    include the post routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from routes import postRoutes
 from routes import userRoutes
 
 
 app = FastAPI()
 
 app.include_router(userRoutes.router ,prefix="/users" ,tags=["Users"])
+app.include_router(postRoutes.router ,prefix="/posts" ,tags=["Post"])
+
 
