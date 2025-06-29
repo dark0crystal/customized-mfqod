8e74a617e781f286dd66edfe08eb86c7ce952308
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-29 18:35:42 +0400

    adding Item model and docs

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,9 @@
-from sqlmodel import SQLModel , Field
+from __future__ import annotations
+from sqlmodel import Relationship, SQLModel , Field
 import uuid
 from enum import Enum
-from typing import Optional
+from typing import List, Optional
+from datetime import datetime, timezone
 # ==========================
 # Database Models Using SQLmodel ORM
 # ==========================
@@ -26,7 +28,12 @@ from typing import Optional
 # Adding table=True tells SQLModel:
 # "This class is a real database table, and it should be created in the database."
 # ==========================
-# 
+# 🧠What is from __future__ import annotations?
+# It’s a special import in Python that changes how type hints are handled.
+# Normally, when you write type hints, Python evaluates them immediately. That means the classes or types you refer to in annotations must be already defined in your code when Python runs.
+# Why use from __future__ import annotations?
+# 🍸 This import tells Python:
+# "Don’t evaluate type hints right away. Instead, store them as strings and evaluate them later only if needed."
 # ==========================
 
 
@@ -51,15 +58,46 @@ class User(SQLModel, table=True):
       # Foreign key and relationship with Role
     role_id: Optional[str] = Field(default=None, foreign_key="role.id")
     role: Optional["Role"] = Relationship(back_populates="users")
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+
+    items: List[Item] = Relationship(back_populates="user")
+
+
 class Role(SQLModel, table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     name: str = Field(index=True, unique=True)
     description: Optional[str] = None
-    
 
-
-
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
 
 
 class Item(SQLModel , table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
+    title:str
+    description:str
+    claims_count:str
+    temporary_deletion:bool = Field(default=False)
+    approval:bool = Field(default=True)
+
+    item_type_id: Optional[str] = Field(default=None, foreign_key="itemtype.id")
+    item_type: Optional[ItemType] = Relationship(back_populates="items")
+
+    user_id: Optional[str] = Field(default=None, foreign_key="user.id")
+    user: Optional["User"] = Relationship(back_populates="items")
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+
+class ItemType(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
+    name: str = Field(index=True, unique=True, nullable=False)
+    description: Optional[str] = None
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+
+
+
