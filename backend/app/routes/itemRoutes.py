c77ad0c8ac00603afa515065438dcdd754db8f42
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-29 20:44:04 +0400

    fix

---
+++ backend/app/routes/itemRoutes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, HTTPException
+
+
+router = APIRouter()
+
+
+# ===========================
+# Add New Item Endpoint
+# ===========================
+# @router.post("add-item")
+# def addNewPost(item:CreateNewItem)
