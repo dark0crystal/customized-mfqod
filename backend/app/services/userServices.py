1bb09d54739534d4e0bee47385557114051e1f2a
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-01 10:37:54 +0400

    user service to create new user in db and check user existence

--- backend/app/services/userServices.py
+++ backend/app/services/userServices.py
@@ -3,6 +3,11 @@ from datetime import datetime, timedelta
 import os
 from dotenv import load_dotenv
 import uuid
+from sqlmodel import Session
+from backend.app.schemas.user_schema import UserRegister
+from models import User
+from sqlmodel import select
+from datetime import datetime, timezone
 # from ldap3 import Server, Connection, ALL, NTLM, SIMPLE
 
 load_dotenv()
@@ -27,9 +32,11 @@ ADusers = [
     {"id":"gfsdaggfdsfs1", "first_name":"omar","last_name":"alomani" , "email": "user1@example.com", "password": "password1wfsda23","role":"manegar"},
     {"id":"gfsdaggfdsfs1", "first_name":"omar","last_name":"alomani" , "email": "admin@example.com", "password": "password1dsfa23","role":"admin"},
 ]
-async def CheckUserExistenceDB(email: str):
-    
-    return   
+
+def CheckUserExistenceDB(session: Session, email: str) -> bool:
+    statement = select(User).where(User.email == email)
+    result = session.exec(statement).first()
+    return result is not None
 
 
 async def CheckUserExistenceAD(email: str):
@@ -41,17 +48,24 @@ async def CheckUserExistenceAD(email: str):
 # this function will create a new user in the local database   
 
 
-def CreateNewUserInDB(ad_user):
-    new_user = {
-        "id": str(uuid.uuid4()),  
-        "first_name": ad_user["first_name"],
-        "last_name": ad_user["last_name"],
-        "email": ad_user["email"],
-        "password": ad_user["password"],
-        "role": ad_user["role"]
-    }
-    DBusers.append(new_user)
-    return new_user
+def create_new_user_in_db(session: Session, user_data: UserRegister, hashed_password: str) -> User:
+    user = User(
+        id=user_data.id or str(uuid.uuid4()),
+        email=user_data.email,
+        password=hashed_password,
+        first_name=user_data.first_name,
+        middle_name=user_data.middle_name,
+        last_name=user_data.last_name,
+        phone_number=user_data.phone_number,
+        status=user_data.status,
+        role_id=None,  # Set role if needed
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc)
+    )
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
 
 # userId , email, role, first_name, last_name
 async def CreateJwtToken(user_id: str, email: str, role: str, first_name: str, last_name: str):
