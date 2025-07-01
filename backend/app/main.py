bc9dc00610bf8c9f7b8e35f629e277e8ba80214e
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-01 09:54:00 +0400

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 from db.database import init_db
 from routes import itemRoutes
@@ -7,7 +6,9 @@ from routes import userRoutes
 
 app = FastAPI()
 
-init_db()
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 app.include_router(userRoutes.router ,prefix="/users" ,tags=["Users"])
 app.include_router(itemRoutes.router ,prefix="/items" ,tags=["Item"])
