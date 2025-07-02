763d4824868cf12ba271fdb198e54ba6f4e164c6
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-02 23:12:16 +0400

    setup new routes and comment some

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from db.database import init_db
-from routes import itemRoutes
-from routes import userRoutes
-
+from routes import userRoutes, roleRoutes, itemRoutes ,authRoutes,ldapAuthRoutes,itemTypeRoutes
 
 app = FastAPI()
 
@@ -10,7 +8,9 @@ app = FastAPI()
 def on_startup():
     init_db()
 
-app.include_router(userRoutes.router ,prefix="/users" ,tags=["Users"])
-app.include_router(itemRoutes.router ,prefix="/items" ,tags=["Item"])
-
-
+app.include_router(userRoutes.router, prefix="/users", tags=["Users"])
+app.include_router(itemRoutes.router, prefix="/items", tags=["Item"])
+app.include_router(roleRoutes.router, prefix="/roles", tags=["Role"])
+# app.include_router(authRoutes.router, prefix="/auth", tags=["Auth"])
+app.include_router(ldapAuthRoutes.router, prefix="/ldap-auth", tags=["LdapAuth"])
+app.include_router(itemTypeRoutes.router, prefix="/item-type", tags=["ItemType"])
