17560adae7617991a94dc9f86cf5332dda05c363
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-02 22:21:31 +0400

    use SQLalchemy instead of SQLmodel

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,6 +1,8 @@
 import os
-from dotenv import load_dotenv        
-from sqlmodel import SQLModel, create_engine, Session  
+from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine
+from models import Base
 
 # =========================
 # Setup database connection 
@@ -14,15 +16,18 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # Create the database engine with echo enabled (logs SQL to console for debugging)
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 # A generator function that yields a database session to use in routes/services
 def get_session():
-    with Session(engine) as session:
+    with SessionLocal() as session:
         yield session
 
-# A utility function that creates all the tables defined in your SQLModel classes
+# A utility function that creates all the tables defined in your SQLAlchemy models
 def init_db():
     try:
-        SQLModel.metadata.create_all(engine)  # Reads all models and creates matching tables
+        Base.metadata.create_all(engine)  # Reads all models and creates matching tables
         print("✅ Tables created successfully.")
     except Exception as e:
         print("❌ Failed to create tables.")
