1d3f452db1484db4aabedcb2e5371fa3b94ba524
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-02 21:54:48 +0400

    add items schema

--- backend/app/schemas/item_schema.py
+++ backend/app/schemas/item_schema.py
@@ -1,21 +1,45 @@
-from pydantic import BaseModel, EmailStr
-from enum import Enum
+# =========================== 
+# Pydantic Schemas
+# ===========================
+
 from datetime import datetime
 from typing import Optional
 
-# Define an Enum for type
-class ItemType(str, Enum):
+from pydantic import BaseModel
+from sqlalchemy import Enum
+
+
+class ItemTypeEnum(str, Enum):
     note = "note"
-    article = "article"
+    article = "article" 
     document = "document"
 
-# Define your schema
 class CreateNewItem(BaseModel):
     title: str
-    userId: str
-    content: str
-    type: ItemType  # Using Enum for 'type'
-    claim_count: int = 0
-    approval: bool = False
-    created_at: Optional[datetime] = None
-    updated_at: Optional[datetime] = None
+    description: str  
+    user_id: str     
+    item_type_id: Optional[str] = None  
+    approval: bool = True
+    temporary_deletion: bool = False
+
+class UpdateItem(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    item_type_id: Optional[str] = None
+    approval: Optional[bool] = None
+    temporary_deletion: Optional[bool] = None
+
+class ItemResponse(BaseModel):
+    id: str
+    title: str
+    description: str
+    claims_count: int
+    temporary_deletion: bool
+    approval: bool
+    item_type_id: Optional[str]
+    user_id: Optional[str]
+    created_at: datetime
+    updated_at: datetime
+    
+    class Config:
+        from_attributes = True
