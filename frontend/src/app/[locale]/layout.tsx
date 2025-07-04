a6a97108d4d85ecdaadee2141b041e332010746d
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-04 23:03:16 +0400

    add navbar to layout

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -4,13 +4,14 @@ import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 import { Alexandria } from "next/font/google";
+import NavBar from "@/components/navbar/Navbar";
 
 
 const alexandria = Alexandria({ 
    subsets: ["latin"],
    weight: ["100","200","300","400","500","600", "700","800","900"],
  });
- 
+
 export const metadata: Metadata = {
   title: "مفقود | MFQOD",
   description:"مفقود",
@@ -30,12 +31,22 @@ export default async function RootLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
+   let direction ="";
+  if(locale == "ar"){
+    direction ="rtl"
+  }else{
+    direction ="ltr"
+  }
   return (
-    <html lang={locale}>
+    <html lang={locale} dir={direction}>
       <body
         className={`${alexandria.className} antialiased`}
       >
-         <NextIntlClientProvider>{children}</NextIntlClientProvider>
+         <NextIntlClientProvider>
+           <NavBar/>
+          {children}
+          </NextIntlClientProvider>
       </body>
     </html>
   );
