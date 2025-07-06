c6886954375aeab11484eb3b7c00d37c48bbf696
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-06 18:24:27 +0400

    Home page

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -13,6 +13,7 @@ import { getTranslations } from "next-intl/server"
 // import { Link } from "@/i18n/navigation"
 import { SearchButton } from "@/components/buttons/SearchButton"
 import FoundButton from "@/components/buttons/FoundButton"
+import CardsSection from "@/components/CardsSection"
 
 export default async function Home() {
   
@@ -71,73 +72,21 @@ export default async function Home() {
           <SearchButton/>
           <FoundButton/>
         </div>
-      
 
         {/* Bottom Right Card */}
         
       </div>
 
     <div>
-      {/* <CardsSection/> */}
+      <CardsSection/>
     </div>
-
-
-    <div>
-      {/* <TrustedBy/> */}
-    </div>
-  
-     
-      {/* <div ref={container}>
-        <Slide src={img5} direction={'left'} left={"-40%"} progress={scrollYProgress}/>
-        <Slide src={img2} direction={'right'} left={"-25%"} progress={scrollYProgress}/>
-        <Slide src={img3} direction={'left'}  left={"-75%"} progress={scrollYProgress}/>
-      </div> */}
-     
 
       <Footer/>
     </main>
   );
 }
 
-// const Slide = (props:any) => {
-//   const direction = props.direction == 'left' ? -1 : 1;
-//   const translateX = useTransform(props.progress, [0, 1], [150 * direction, -150 * direction])
-//   return (
-//     <motion.div style={{x: translateX, left: props.left}} className="relative flex whitespace-nowrap">
-//       <Phrase src={props.src}/>
-//       <Phrase src={props.src}/>
-//       <Phrase src={props.src}/>
-//     </motion.div>
-//   )
-// }
-
-// const Phrase = ({src}:any) => {
-
-//   return (
-//     <div className={'px-5 flex gap-5 items-center'}>
-//       <p className='text-[6vw] text-black/44 font-bold'>مغيب شي ؟ مفقود بيساعدك</p>
-//       <span className="relative h-[7.5vw] aspect-[4/2] rounded-full overflow-hidden">
-//         <Image style={{objectFit: "cover"}} src={src} alt="image" fill/>
-//       </span>
-//     </div>
-//   )
-// }
 
 
-// async function FoundButton() {
-//   const t = await getTranslations("HomePage")
-//   return (
-//     <Link href="/report-found-item" className=" w-full md:w-[200px] bg-gradient-to-r from-white to-white border border-black p-4 rounded-xl text-black font-semibold text-center hover:shadow-lg transform hover:scale-105 transition-all duration-300 cursor-pointer">
-//       <h1>{t("report")}</h1>
-//     </Link>
-//   );
-// }
 
-// async function SearchButton() {
-//   const t = await getTranslations("HomePage")
-//   return (
-//     <Link href="/search" className="w-full md:w-[200px]  bg-gradient-to-r from-[#2196f3] to-[#2f7ce1] p-4 rounded-xl text-white font-semibold text-center hover:shadow-lg transform hover:scale-105 transition-all duration-300 cursor-pointer">
-//       {t("search")}
-//     </Link>
-//   );
-// }
+
