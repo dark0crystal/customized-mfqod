c04dc652f7a725f79fd4933357cf3aa8627d4abe
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-06 18:24:11 +0400

    add colored background

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -43,6 +43,8 @@ export default async function RootLayout({
       <body
         className={`${alexandria.className} antialiased`}
       >
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(70,130,180,0.3),rgba(255,255,255,0))]"></div>
+
          <NextIntlClientProvider>
            <NavBar/>
           {children}
