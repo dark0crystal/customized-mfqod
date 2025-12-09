3564087a488b9611a034113e5ff802c1ad96275f
ALMardasDEV <albusaidi9094@gmail.com>
2025-12-09 09:18:22 +0400

    use the dynamic localization in layout

--- frontend/src/app/[locale]/(protected)/dashboard/layout.tsx
+++ frontend/src/app/[locale]/(protected)/dashboard/layout.tsx
@@ -1,19 +1,11 @@
 // app/admin/layout.tsx
 import type { Metadata } from "next";
-import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { hasLocale } from "next-intl";
 import "./globals.css"
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import SideNavbar from "./SideNavbar";
-import { Alexandria } from "next/font/google";
 import { PermissionsProvider } from "@/PermissionsContext";
-
-
-const alexandria = Alexandria({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -33,34 +25,21 @@ export default async function AdminLayout({
     notFound();
   }
 
-  let direction = "";
-  if (locale == "ar") {
-    direction = "rtl";
-  } else {
-    direction = "ltr";
-  }
-
   return (
-    <html lang={locale} dir={direction}>
-      <body className={`${alexandria.variable}  antialiased bg-gray-50`}>
-        <NextIntlClientProvider>
-          <PermissionsProvider>
-          <div className="flex h-screen overflow-hidden">
-            {/* Sidebar */}
-            <SideNavbar/>
+    <PermissionsProvider>
+      <div className="flex h-screen overflow-hidden bg-gray-50">
+        {/* Sidebar */}
+        <SideNavbar/>
 
-            {/* Content Area */}
-            <div className="flex flex-col flex-1 overflow-y-auto">
-              {/* Topbar */}
-              {/* <AdminTopbar /> */}
+        {/* Content Area */}
+        <div className="flex flex-col flex-1 overflow-y-auto">
+          {/* Topbar */}
+          {/* <AdminTopbar /> */}
 
-              {/* Page Content */}
-              <main className="p-6">{children}</main>
-            </div>
-          </div>
-          </PermissionsProvider>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+          {/* Page Content */}
+          <main className="p-6">{children}</main>
+        </div>
+      </div>
+    </PermissionsProvider>
   );
 }
